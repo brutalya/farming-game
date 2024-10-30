09b681de9ad24f390db34542af4b98496e65a1f8
Rus <rasl2222@icloud.com>
2024-10-30 17:56:35 +0100

    script for tg

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 // src/api.js
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://api.gryadki.click/api';
 const TOKEN_KEY = 'auth_token';
 
 // Centralized token storage
--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import './index.css';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<script async src="https://telegram.org/js/telegram-web-app.js"></script>
-
 		<App />
 	</React.StrictMode>,
 	document.getElementById('root')
